01c2b741ff96698a8b5bf6b58b34e7b12c7e6836
Abhisek-Ray99 <rickyrayabhisek99@gmail.com>
2020-11-16 00:41:37 +0530

    change syle in Title and Navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Show from "./pages/Show";
+import { ThemeProvider } from "styled-components";
 import Starred from "./pages/Starred";
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
       <Switch>
         <Route exact path="/">
           <Home />
@@ -20,6 +30,8 @@ function App() {
           <div>Not found</div>
         </Route>
       </Switch>
+    </ThemeProvider>
+      
   );
 }
 
